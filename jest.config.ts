5a926a739e9dbcb3b760d97af08c0daf43d100e3
Abdul Wahab <akramabdulwahab0321@gmail.com>
2024-12-19 22:53:43 +0500

    Configured playwright also improved jest settings

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ import { compilerOptions } from './tsconfig.json';
 
 export default {
   ...presets.createCjsPreset(),
+  testMatch: ['<rootDir>/e2e/jest/**/*.spec.ts'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
 } satisfies JestConfigWithTsJest;
